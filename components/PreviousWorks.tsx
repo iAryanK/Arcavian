d2cf21c7d4f3d572d6c8608b5f1cf1b736c1063d
Aryan <aryaninmail@gmail.com>
2024-07-27 15:13:22 +0530

    initial setup

---
+++ components/PreviousWorks.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const PreviousWorks = (props: Props) => {
+  return <div>PreviousWorks</div>;
+};
+
+export default PreviousWorks;
---
+++ components/Process.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Process = (props: Props) => {
+  return <div>Process</div>;
+};
+
+export default Process;
